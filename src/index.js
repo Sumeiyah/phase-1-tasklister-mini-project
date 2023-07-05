7ee34bc7c824f62758a5187cf81adf9a4a80f163
sumeiyah <sumeyafarah171400>
2023-07-05 10:07:41 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+//let priority = document.querySelector("#priority");
+
+
+//btn.addEventListener("click",function () {
+    //console.log("");
+    
+//});
+
+const priority = document.getElementsByTagName('option');
+const submitButton = document.getElementsByTagName('input');
+option.addEventListener('click', function () {
+    const val =option.value;
+    console.log('your priority', val);
+    
+});
+submitButton.addEventListener('click',  function() {
+    console.log("submitted the form");
 });
